6e59a2ecd9aa2d9e906d7738c3be9090b780605c
Velliraja <vellirajamariyappan0706@gmail.com>
2023-01-05 13:44:57 +0530

    modified by velli

--- src/test/java/org/emp/MasterProject.java
+++ src/test/java/org/emp/MasterProject.java
@@ -9,5 +9,10 @@ public class MasterProject {
 	private void tc2() {
 		System.out.println("2");
 	}
-	
+	private void raja() {
+		System.out.println("raja");
+	}
+	private void kavi() {
+		System.out.println("kavi");
+		}
 }
